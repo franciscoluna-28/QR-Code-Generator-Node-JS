c3dd71a088f74910d1ccdde4587ec509fe6540df
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-03-07 11:11:04 -0400

    updated settings

--- back/app.js
+++ back/app.js
@@ -1,48 +1,56 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
 var body_parser_1 = __importDefault(require("body-parser"));
+var dotenv = __importStar(require("dotenv"));
 var qrcode_1 = __importDefault(require("qrcode"));
 var path = require('path');
-// initializing express
+var regexVerification_1 = require("./utilities/regexVerification");
+// initializing app dependencies
 var app = (0, express_1["default"])();
-// setting ejs view engine
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
-// using a middleware called body parser
 app.use(body_parser_1["default"].urlencoded({ extended: false }));
 app.use(body_parser_1["default"].json());
-// exposing the dist folder so we can style properly
+dotenv.config();
+// Exposing dist
 app.use(express_1["default"].static(path.join(__dirname, '../dist')));
-// routing the index.ejs template
+// Index route
 app.get("/", function (req, res) {
     res.render("index.ejs");
 });
-// creating a post request to convert the URL into a QR Code
+// Post request to generate the url
 app.post("/scan", function (req, res) {
     var url = req.body.url;
-    // Verify if the URL is valid using regex
-    var validUrlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i;
-    if (!validUrlRegex.test(url)) {
-        res.send("Invalid URL!");
-        return;
-    }
-    // Let's check if the URL is empty
-    if (typeof url !== "string" || url.trim() === "") {
-        res.send("Empty data!");
-        return;
-    }
-    // If everything is fine, generate the QR Code
-    qrcode_1["default"].toDataURL(url, function (err, src) {
-        if (err)
-            res.send("Something went wrong");
-        // returning the data if everything is fine
-        res.render("scan", { src: src });
-    });
+    // Regex verification check
+    (0, regexVerification_1.isValidRegexForUrl)(url, res, qrcode_1["default"]);
 });
 // setting up the port for listening requests
-var port = 5000;
+var port = (process.env["SERVER_PORT"]);
 app.listen(port, function () { return console.log("Server at ".concat(port)); });
