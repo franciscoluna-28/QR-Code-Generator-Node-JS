dfb5913c20733dede22602c0aa6336abf0ff27d3
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-03-12 20:38:06 -0400

    added new style rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,23 @@ module.exports = {
       colors: {
         "main": "#1AA6B7",
         "secondary": "#F56A79"
-      }
+      },
+      screens: {
+                'max-1535': { 'max': '1535px' },
+                // => @media (max-width: 1535px) { ... }
+
+                'max-1279': { 'max': '1279px' },
+                // => @media (max-width: 1279px) { ... }
+
+                'max-1023': { 'max': '1023px' },
+                // => @media (max-width: 1023px) { ... }
+
+                'max-767': { 'max': '767px' },
+                // => @media (max-width: 767px) { ... }
+
+                'max-639': { 'max': '639px' },
+                // => @media (max-width: 639px) { ... }
+            },
     },
   },
   plugins: [],
